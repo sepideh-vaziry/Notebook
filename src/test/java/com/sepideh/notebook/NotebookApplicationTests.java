ff3cb494d026ec3db72048db05e87ba51be827b1
Sepideh Vaziry <sepideh.vaziry@gmail.com>
2021-03-05 16:47:36 +0330

    First commit (create project)

---
+++ src/test/java/com/sepideh/notebook/NotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sepideh.notebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
