ee1eaa982b5ba622097696537e15d95f9f683b20
Sepideh Vaziry <sepideh.vaziry@gmail.com>
2021-03-14 22:01:51 +0330

    Complete jwt util

--- src/main/java/com/sepideh/notebook/jwt/JwtUtil.java
+++ src/main/java/com/sepideh/notebook/jwt/JwtUtil.java
@@ -1,11 +1,14 @@
 package com.sepideh.notebook.jwt;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class JwtUtil {
@@ -33,8 +36,51 @@ public class JwtUtil {
     }
 
     //******************************************************************************************************************
-//    public String getUsernameFromToken(String token) {
-//
-//    }
+    /**
+     * Retrieve username from jwt token
+     * @param token jwt token
+     * @return username
+     */
+    public String getUsernameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    //******************************************************************************************************************
+    /**
+     * check if the token has expired
+     * @param token
+     * @return
+     */
+    public Boolean isTokenExpired(String token) {
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date(System.currentTimeMillis()));
+    }
+
+    //******************************************************************************************************************
+    /**
+     * Retrieve expiration date from jwt token
+     * @param token Jwt token
+     * @return token expiration date
+     */
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    //******************************************************************************************************************
+    /**
+     * Retrieve {T} from claims
+     */
+    private <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    //******************************************************************************************************************
+    /**
+     * Retrieve any information from token we will need the secret key
+     */
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+    }
 
 }
